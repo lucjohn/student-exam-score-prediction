b22a445017bfd89e78f4625993b867cce1f92b59
john <johnluc0718@gmail.com>
2026-05-13 17:51:14 -0400

    data ingestion + notebook"

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,8 @@
 import sys
 from typing import Any
 import logging
+from src.logger import logging
+
 
 def error_message_details(error, error_detail: Any):
 
@@ -23,12 +25,6 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
 
-if __name__ == "__main__":
-    try:
-        a = 1 / 0
-    except Exception as e:
-        logging.info("An error occurred: {}".format(str(e)))
-        raise CustomException(e, sys)
 
 
     
