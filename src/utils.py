5bd44ea43b828fb1428f34ce579a701dcd21422f
john <johnluc0718@gmail.com>
2026-05-17 16:20:18 -0400

    hyperparam training

--- src/utils.py
+++ src/utils.py
@@ -5,8 +5,10 @@ import numpy as np
 import pandas as pd
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
+
 
 def save_object(file_path, obj):
     try:
@@ -21,12 +23,16 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
 
 
-def evaluate_model(X_training_set, Y_Training_set, X_test_set, Y_test_set, models):
+def evaluate_model(X_training_set, Y_Training_set, X_test_set, Y_test_set, models, params):
     try:
         report = {}
         for i in range(len(models)):
             model = list(models.values())[i] 
+            para = params[list(models.keys())[i]]
             model.fit(X_training_set, Y_Training_set)
+
+            gs = GridSearchCV(model, para, cv=3) 
+            gs.fit(X_training_set, Y_Training_set)
             
             Y_training_prediction = model.predict(X_training_set)
             Y_test_prediction = model.predict(X_test_set)
